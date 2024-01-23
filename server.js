48d092b25ca27b1189719de8c1fd6ea5b3263795
Lloydmedina <79982709+Lloydmedina@users.noreply.github.com>
2024-01-23 12:29:14 +0800

    added market_payment_schedule module

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use("/api/market_property_setup", market_property_setup);
 app.use("/api/market_property_section", market_property_section);
 app.use("/api/market_property_lot_unit", market_property_lot_unit);
 app.use("/api/market_property_floor_block", market_property_floor_block);
-// app.use("/api/market_payment_schedule", market_payment_schedule);
+app.use("/api/market_payment_schedule", market_payment_schedule);
 // app.use("/api/market_payment_addons", market_payment_addons);
 // app.use("/api/market_inspect_logs", market_inspect_logs);
 
