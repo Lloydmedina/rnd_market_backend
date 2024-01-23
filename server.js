b3b0aeb024baf06107180e2153a062f07f8aa953
Lloydmedina <79982709+Lloydmedina@users.noreply.github.com>
2024-01-23 11:33:54 +0800

    added market property type and property tenant module

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const users = require('./app/routes/user');
 const employee = require('./app/routes/employee');
 const person = require('./app/routes/person');
 const office = require('./app/routes/office');
+
+const market_property_type = require('./app/routes/market_property_type');
+const market_property_tenant_occupant = require('./app/routes/market_property_tenant_occupant');
+const market_property_status = require('./app/routes/market_property_status');
+const market_property_setup = require('./app/routes/market_property_setup');
+const market_property_section = require('./app/routes/market_property_section');
+const market_property_tenant = require('./app/routes/market_property_tenant');
+const market_property_lot_unit = require('./app/routes/market_property_lot_unit');
+const market_property_floor_block = require('./app/routes/market_property_floor_block');
+const market_payment_schedule = require('./app/routes/market_payment_schedule');
+const market_payment_addons = require('./app/routes/market_payment_addons');
+const market_inspect_logs = require('./app/routes/market_inspect_logs');
 app.use(cors());
 
 // parse requests of content-type - application/json
@@ -23,6 +35,10 @@ app.use(
     httpOnly: true,
   })
 );
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to RND API." });
+});
 // users route
 app.use("/api/users", users);
 
@@ -35,10 +51,18 @@ app.use("/api/person", person);
 //office route
 app.use("/api/office", office);
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to RND API." });
-});
+//market routes
+app.use("/api/market_property_type", market_property_type);
+app.use("/api/market_property_tenant", market_property_tenant);
+// app.use("/api/market_property_tenant_occupant", market_property_tenant_occupant);
+// app.use("/api/market_property_status", market_property_status);
+// app.use("/api/market_property_setup", market_property_setup);
+// app.use("/api/market_property_section", market_property_section);
+// app.use("/api/market_property_lot_unit", market_property_lot_unit);
+// app.use("/api/market_property_floor_block", market_property_floor_block.js);
+// app.use("/api/market_payment_schedule", market_payment_schedule);
+// app.use("/api/market_payment_addons", market_payment_addons);
+// app.use("/api/market_inspect_logs", market_inspect_logs);
 
 //auth route
 require('./app/routes/auth.routes')(app);
