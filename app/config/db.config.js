0ffea67a09eaccc1ba2ac21c57fd0f05e9a45ac1
Lloydmedina <79982709+Lloydmedina@users.noreply.github.com>
2024-02-01 05:44:07 +0800

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -8,9 +8,18 @@ module.exports = {
     PORT: "3309",
     AUTHDB : 'user',
     dialect: "mysql",
-    
+   
+    // HOST: "143.198.218.188",
+    // USER: "root",
+    // PASSWORD: "L3tM3!n@123654",
+    // DB: "general_db",
+    // MARKET_DB: 'market_db',
+    // HR_DB : 'hr_db',
+    // PORT: "3309",
+    // AUTHDB : 'user',
+    // dialect: "mysql",
     pool: {
-      max: 5,
+      max: 10,
       min: 0,
       acquire: 30000,
       idle: 10000
