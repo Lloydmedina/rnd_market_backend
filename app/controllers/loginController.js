b368d712f2794b5672f86dfb6d27308f2deb6906
Lloydmedina <79982709+Lloydmedina@users.noreply.github.com>
2024-02-02 04:52:16 +0800

    auth bug fix

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -16,15 +16,16 @@ async function login(req, res) {
 
         //const reqJson = db.filterReqJson(req.body, UserAuthModel);
 
-        const queryString = `SELECT * FROM users WHERE username = '${username}'`;
+        const queryString = `SELECT * FROM user WHERE username = '${username}'`;
 
         const user_ = await db.executeQuery(queryString);
+        
 
       if (user_.length > 0) {
         const isValid = await bcrypt.compare(password,user_[0]['password']);
             if (isValid) {
               res.status(200);
-          res.send({status: "logged in", success: true});
+                res.send({ user_ });
             } else {
               res.status(400);
             res.send({status: "Password incorrect", success: false});
